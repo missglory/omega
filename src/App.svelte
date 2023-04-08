a60e1a95cf542d5a2e4cfbbf0b9b81951c0ef4a9
Dmitriy Lyuosev <xhaustful@gmail.com>
2023-04-09 02:45:46 +0300

    feat: Working sigma-container

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,42 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-	export let name: string;
-	import {main} from './sigma/examples/chrome-deps/index';
+  import { onMount } from "svelte";
+  export let name: string;
+  import { main } from "./sigma/examples/chrome-deps/index";
 
-	onMount(async () => {
-		main();
-	});
+  onMount(async () => {
+    main();
+  });
 </script>
 
 <main>
-	<h1>{name}!</h1>
+  <h1>{name}!</h1>
+  <div id="sigma-container" />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+
+  #sigma-container {
+    width: 800px;
+    height: 600px;
+		position: absolute;
+  }
 </style>
